b6437a71a0a311a6cfb449160e90124cabc20f50
bunny-poooh <shubhu.patel1104@gmail.com>
2019-12-11 22:59:25 +0530

    db host change

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'u395025619_turnweldengineers' );
 define( 'DB_PASSWORD', 'u395025619_turnweldengineers' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
